e503b6314f8a6ef75bee52c18997ddb60980ba5f
Yasset Perez-Riverol <ypriverol@gmail.com>
2020-02-17 20:31:42 +0000

    circleci

---
+++ .circleci/filecheck.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [[ -z "${CIRCLE_PULL_REQUEST}" ]];
+then
+	echo "This is not a pull request, no PHPCS needed."
+	exit 0
+else
+	echo "This is a pull request, continuing"
+fi
+
+if [[ -z $GITHUB_TOKEN ]];
+then
+	echo "GITHUB_TOKEN not set"
+	exit 1
+fi
+
+regexp="[[:digit:]]\+$"
+PR_NUMBER=`echo $CIRCLE_PULL_REQUEST | grep -o $regexp`
+
+url="https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/$PR_NUMBER"
+
+target_branch=$(curl -s -X GET -G \
+$url \
+-d access_token=$GITHUB_TOKEN | jq '.base.ref' | tr -d '"')
+
+echo "Resetting $target_branch to where the remote version is..."
+git checkout -q $target_branch
+
+git reset --hard -q origin/$target_branch
+
+git checkout -q $CIRCLE_BRANCH
+
+echo "Getting list of changed files..."
+changed_files=$(git diff --name-only $target_branch..$CIRCLE_BRANCH -- '*sdrf*')
+echo $changed_files > files.txt
+
+if [[ -z $changed_files ]]
+then
+	echo "There are no files to check."
+	exit 0
+fi
+
+echo "Running validation..."
+while read -r line; do  if [[ $line == *"sdrf"* ]]; then python ${VIRTUAL_ENV}/lib/python${TRAVIS_PYTHON_VERSION}/site-packages/sdrfcheck/sdrfchecker.py validate-sdrf --sdrf_file $line ; fi; done < files.txt
+
