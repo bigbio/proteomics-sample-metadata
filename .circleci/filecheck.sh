937589e73c1eb6bdd48dd85c366ba986e397c642
Yasset Perez-Riverol <ypriverol@gmail.com>
2020-02-21 15:39:42 +0000

    Update filecheck.sh

--- .circleci/filecheck.sh
+++ .circleci/filecheck.sh
@@ -5,7 +5,7 @@ then
 	echo "This is not a pull request, no PHPCS needed."
 	find ./ -name "sdrf.txt" > files.txt
         more files.txt
-	while read -r line; do  if [[ $line == *"sdrf"* ]]; then python /usr/local/lib/python3.6/site-packages/sdrfcheck/sdrfchecker.py validate-sdrf --sdrf_file $line ; fi; done < files.txt
+	while read -r line; do  if [[ $line == *"sdrf"* ]]; then echo "Fille analyzed --" $line; python /usr/local/lib/python3.6/site-packages/sdrfcheck/sdrfchecker.py validate-sdrf --sdrf_file $line ; fi; done < files.txt
         exit 0 
 else
 	echo "This is a pull request, continuing"
