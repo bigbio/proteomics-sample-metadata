0ea1698a0cbd2e4186f547926371c1bf5e31d2da
Yasset Perez-Riverol <ypriverol@gmail.com>
2020-02-21 15:32:09 +0000

    Update filecheck.sh

--- .circleci/filecheck.sh
+++ .circleci/filecheck.sh
@@ -3,7 +3,7 @@
 if [[ -z "${CIRCLE_PULL_REQUEST}" ]];
 then
 	echo "This is not a pull request, no PHPCS needed."
-	git pull $CIRCLE_BRANCH
+	cd $CIRCLE_WORKING_DIRECTORY
 	ls -R annotated-projects/ | grep "srdf.txt" > files.txt
         more files.txt
 	while read -r line; do  if [[ $line == *"sdrf"* ]]; then python /usr/local/lib/python3.6/site-packages/sdrfcheck/sdrfchecker.py validate-sdrf --sdrf_file $line ; fi; done < files.txt
