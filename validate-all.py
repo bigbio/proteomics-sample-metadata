73c63ebac6bdc9ce7bca2a74050bc87e7b738214
Lev Levitsky <lev.levitsky@phystech.edu>
2020-05-12 02:51:43 +0300

    Add a script that runs SDRF validator on all annotated projects and prints a summary

---
+++ validate-all.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import os
+import glob
+import sys
+from sdrf_pipelines.sdrf import sdrf, sdrf_schema
+DIR = 'annotated-projects'
+VALIDATE = ['parse_sdrf', 'validate-sdrf']
+projects = os.listdir(DIR)
+
+
+def get_template(df):
+    """Extract organism information and pick a template for validation"""
+    organisms = df['characteristics[organism]'].unique()
+    if organisms.size > 1:
+        return None
+    organism = organisms[0].lower()
+    if organism == 'homo sapiens':
+        return sdrf_schema.HUMAN_TEMPLATE
+
+
+def main():
+    status = []
+    for project in projects:
+        sdrf_files = glob.glob(os.path.join(DIR, project, '*.tsv'))
+        if sdrf_files:
+            result = 'OK'
+            for sdrf_file in sdrf_files:
+                df = sdrf.SdrfDataFrame.parse(sdrf_file)
+                errors = df.validate(sdrf_schema.DEFAULT_TEMPLATE)
+                if errors:
+                    result = 'Failed basic validation'
+                    break
+                else:
+                    template = get_template(df)
+                    if template:
+                        errors = df.validate(template)
+                        if errors:
+                            result = 'Failed validation for {}'.format(template)
+                            break
+        else:
+            result = 'SDRF file not found'
+        status.append(result)
+        print(project, result, sep='\t')
+    errors = 0
+    print('Final results:')
+    for project, result in zip(projects, status):
+        if result != 'OK':
+            errors += 1
+    print('Total: {} projects checked, {} had validation errors.'.format(len(projects), errors))
+    return errors
+
+
+if __name__ == '__main__':
+    out = main()
+    sys.exit(out)
