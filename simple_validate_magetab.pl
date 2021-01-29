a1045c63dc1255cf85ba674b32aa084f7fe5437e
ypriverol <ypriverol@gmail.com>
2021-01-29 11:23:25 +0000

    minor change in the sdrf

--- simple_validate_magetab.pl
+++ simple_validate_magetab.pl
@@ -6,12 +6,43 @@
 
 use strict;
 use warnings;
+use File::Path;
 use 5.10.0;
 
 use Bio::MAGETAB::Util::Reader;
+use Bio::MAGETAB::Util::Writer::GraphViz;
+require Bio::MAGETAB::Util::Writer;
+
+
 my $reader = Bio::MAGETAB::Util::Reader->new({
    idf            => $ARGV[0],
    relaxed_parser => 0,
 });
- 
-my $magetab = $reader->parse();
+
+# Parse the IDF and any associated SDRFs
+warn("Parsing MAGE-TAB document...\n");
+my ( $inv, $magetab ) = $reader->parse();
+
+# Attempt to use GraphViz to draw the experimental design graph.
+warn("Attempting to generate a graph visualisation...\n");
+open ( my $fh, '>', 'graph_file_sdrf.png' )
+  or die("Error: Unable to open output file: $!");
+  my $writer = Bio::MAGETAB::Util::Writer::GraphViz->new({
+        sdrfs => [ $inv->get_sdrfs() ],
+  });
+  my $g = $writer->draw();
+  print $fh $g->as_png();
+
+warn("Attempting to write out a new set of MAGE-TAB documents...\n");
+
+mkpath( 'new_path' );
+chdir( 'new_path' );
+
+my $writer = Bio::MAGETAB::Util::Writer->new({
+  magetab => $magetab,
+});
+
+$writer->write();
+
+warn("Done.\n");
+
